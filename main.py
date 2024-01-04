e0e2e38b42fe3bcbd9e6deeea4026736d8ada304
Vári Márton <Vri.Mrton@wattay.local>
2024-01-04 13:56:00 +0100

    push nem lesz

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 if __name__ == '__main__':
     print("legyen zene legyen tánc")
     print("folytatás kódja")
-    print("ez csak egy commit lesz, de nem push-olom")
     print("na majd MOST pusholom majd fel")
