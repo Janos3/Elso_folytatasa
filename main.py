ace0391a30317a9c7d992492599ffc62d14cce90
Vári Márton <Vri.Mrton@wattay.local>
2024-01-04 13:53:53 +0100

    na EZT már push-olom

--- main.py
+++ main.py
@@ -2,3 +2,4 @@ if __name__ == '__main__':
     print("legyen zene legyen tánc")
     print("folytatás kódja")
     print("ez csak egy commit lesz, de nem push-olom")
+    print("na majd MOST pusholom majd fel")
